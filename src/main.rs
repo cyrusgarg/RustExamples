dbde55efb3a5bf7b2d29626823e3075eab97c187
Cyrus Garg <cyrus1@ualberta.ca>
2025-06-15 19:49:09 -0600

    added ownership,borrow & references example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,33 @@
 mod utils;
+use std::fmt::Debug;
+
+struct User{
+    active:bool,
+    username:String,
+    email:String,
+    sign_in_count: u64,
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+impl Debug for Rectangle {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Rectangle {{ width: {}, height: {} }}", self.width, self.height)
+    }
+    // fn area(&self) -> u32 {
+    //     self.width * self.height
+    // }
+}
+
+struct NoShape;
+impl NoShape {
+    fn area(&self) -> u32 {
+        0
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     utils::greet("Cyrus");
@@ -44,15 +73,57 @@ fn main() {
     let number=42;
     let num_str=number.to_string();
     println!("Number as string: {}",num_str);
+    update_string();
 
-    
+    let mut my_string = String::from("Hello, Rust!");
+    my_string=takes_ownership(my_string);
+    println!("{}", my_string); // This will cause an error because ownership has been moved
+    // Borrowing example
+    borrowing_example(&mut my_string);
+    println!("After borrowing: {}", my_string); // This is valid because we borrowed, not moved
 
+    let user1=User{
+        active:true,
+        username: String::from("Cyrus"),
+        email: String::from("cyrus1@ualberta.ca"),
+        sign_in_count:1,
+    };
+    println!("User 1 username: {:?}",user1.username);
+
+    let rect= Rectangle {
+        width: 30,
+        height: 50,
+    };
+    // println!("Rectangle area: {}", rect.area());
+    println!("Rectangle: {:?}", rect); // Using the Debug trait to print the rectangle
+
+    let rect=NoShape;
+    println!("NoShape area: {}", rect.area()); // Calling the area method on NoShape
 }
 
-fn add(x:i32,y:i32)->i32{
+fn takes_ownership(s:String)->String{
+    println!("Taking ownership of: {}",s);
+    s // Ownership is moved here
+}
+
+fn borrowing_example(s:&mut String) {
+    s.push_str(" - borrowed");
+    println!("Borrowing: {}", s);
+}
+
+fn add(x:i32,y:i32)->i32{   
     x+y
 }
 
 fn multiply(a:f64,b:f64)->f64{
     a*b
+}
+
+fn update_string() {
+    let mut my_string = String::from("Hello");
+    println!("Capacity: {}", my_string.capacity());
+    println!("Length: {}", my_string.len());
+    println!("Pointer: {:p}", my_string.as_ptr());
+    my_string.push_str(", Rust!");
+    println!("Updated string: {}", my_string);
 }
